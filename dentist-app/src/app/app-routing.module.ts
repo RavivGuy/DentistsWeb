2bc28cab57c5e8c6d721bb0eecef4e7115747685
guy raviv <guyraviv1997@gmail.com>
2018-11-09 00:07:44 +0200

    Adding carousel to home page

--- dentist-app/src/app/app-routing.module.ts
+++ dentist-app/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
+import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{path: '', component: HomeComponent}] ;
+const routes: Routes = [{path: '', component: HomeComponent},
+{path: 'about', component: AboutComponent}] ;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
