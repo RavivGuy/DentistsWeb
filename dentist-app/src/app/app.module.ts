c5cac1d1bc3e8077f5bb8044ceaf1c7eb054ca20
guy raviv <guyraviv1997@gmail.com>
2019-01-12 19:21:24 +0200

    Add ng-toolkit/universal

--- dentist-app/src/app/app.module.ts
+++ dentist-app/src/app/app.module.ts
@@ -1,3 +1,7 @@
+import { NgtUniversalModule } from '@ng-toolkit/universal';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -14,11 +18,16 @@ import { ClinikComponent } from './clinik/clinik.component';
     AboutComponent,
     ClinikComponent
   ],
-  imports: [
-    BrowserModule,
+  imports:[
+ CommonModule,
+NgtUniversalModule,
+ 
+ TransferHttpCacheModule,
+HttpClientModule,
+ 
+    
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
 })
 export class AppModule { }
