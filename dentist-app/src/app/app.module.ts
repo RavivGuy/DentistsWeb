380a958764db39851496842202c373012723d589
guy raviv <guyraviv1997@gmail.com>
2019-04-27 15:38:15 +0300

    Add contact

--- dentist-app/src/app/app.module.ts
+++ dentist-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { TransferHttpCacheModule } from '@nguniversal/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,7 @@ import { RecommendationsComponent } from './recommendations/recommendations.comp
   imports: [
     CommonModule,
     NgtUniversalModule,
-
+    FormsModule,
     TransferHttpCacheModule,
     HttpClientModule,
 
--- dentist-app/src/app/contact/contact.component.ts
+++ dentist-app/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -9,11 +10,32 @@ export class ContactComponent implements OnInit {
   latitude: number;
   longitude: number;
 
+  @ViewChild('f') signupForm: NgForm;
+  contactRequest = '';
+  user = {
+    username: '',
+    email: '',
+    phone: '',
+    contactRequest: '',
+  };
+
+  submitted = false;
+
   constructor() { }
 
   ngOnInit() {
     this.latitude = 32.306956;
     this.longitude = 34.868919;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.phone = this.signupForm.value.userData.phone;
+    this.user.contactRequest = this.signupForm.value.contactRequest;
+
+    this.signupForm.reset();
   }
 
 }
